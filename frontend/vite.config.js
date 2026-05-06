065e8ba4d8992672ef7e0ea1786cdd8e0fcea122
SofiaAyalanis <sofi-aaa@hotmail.com>
2026-05-06 07:27:00 -0600

    a

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,45 +1,49 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // El simbolo '@' apunta a la carpeta 'src'
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
-  },
-  // Reduce tamano
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('@mui') || id.includes('@emotion')) return 'vendor-mui';
-            if (id.includes('react')) return 'vendor-react';
-            if (id.includes('recharts')) return 'vendor-charts';
-            if (id.includes('swiper')) return 'vendor-swiper';
-            
-            return 'vendor-others';
-          }
+    plugins: [react()],
+    resolve: {
+        alias: {
+            // El simbolo '@' apunta a la carpeta 'src'
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
-      },
     },
-    minify: 'esbuild',
-    chunkSizeWarningLimit: 1000,
-  },
-  // Optimiza
-  optimizeDeps: {
-    include: [
-      '@emotion/react',
-      '@emotion/styled',
-      'swiper',
-      'react-router-dom',
-    ],
-  },
+    server: {
+        port: 5173,
+        strictPort: true,
+    },
+    // Reduce tamano
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+        /* rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        if (id.includes("@mui") || id.includes("@emotion"))
+                            return "vendor-mui";
+                        if (id.includes("react")) return "vendor-react";
+                        if (id.includes("recharts")) return "vendor-charts";
+                        if (id.includes("swiper")) return "vendor-swiper";
+
+                        return "vendor-others";
+                    }
+                },
+            },
+        }, */
+        minify: "esbuild",
+        chunkSizeWarningLimit: 1000,
+    },
+    // Optimiza
+    optimizeDeps: {
+        include: [
+            "@emotion/react",
+            "@emotion/styled",
+            "swiper",
+            "react-router-dom",
+        ],
+    },
 });
