395fee2e8aa046ae405f8c7b8b4afde91b3fed09
SofiaAyalanis <sofi-aaa@hotmail.com>
2026-05-05 22:03:00 -0600

    a

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 
-import { fileURLToPath } from "url";
+/* import { fileURLToPath } from "url";
+const file
+ */
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
