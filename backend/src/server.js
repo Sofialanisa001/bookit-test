1827345af883c3ff601b92fe3d24935ac5639a8d
SofiaAyalanis <sofi-aaa@hotmail.com>
2026-05-05 22:22:15 -0600

    a

--- backend/src/server.js
+++ backend/src/server.js
@@ -47,12 +47,12 @@ app.use("/api/reportes", reporteRoutes);
 
 // PARA SERVIR REACT Y BACK EN PRODUCCIÓN JUNTOS
 if (process.env.NODE_ENV === "production") {
-    const frontendPath = path.join(process.cwd(), "src", "public");
+    const frontendPath = path.join(__dirname, "public");
 
     app.use(express.static(frontendPath));
 
     app.get(/.*/, (req, res) => {
-        res.sendFile(path.resolve(frontendPath, "index.html"));
+        res.sendFile(path.join(frontendPath, "index.html"));
     });
 }
 
